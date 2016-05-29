7afd227dacd565f608671bf0a08ee427740538b5
Meeks91 <meekman2010@gmail.com>
2016-05-29 12:50:13 +0100

    Update FABRevealLayout.java

--- library/src/main/java/com/truizlop/fabreveallayout/FABRevealLayout.java
+++ library/src/main/java/com/truizlop/fabreveallayout/FABRevealLayout.java
@@ -130,7 +130,7 @@ public class FABRevealLayout extends RelativeLayout {
         params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
         params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            params.rightMargin = dipsToPixels(16);
+            params.rightMargin = dipsToPixels(0.4);
             params.topMargin = dipsToPixels(20);
         }
         fab.bringToFront();
